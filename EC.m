f345f6f903bf55f6cb1a99a95b8116955b95eedd
cristivlad21 <103741157+cristivlad21@users.noreply.github.com>
2023-03-31 00:10:50 +0300

    Add files via upload

---
+++ EC.m
@@ -0,0 +1,18 @@
+clear all;
+close all;
+
+VAl=20;
+A=0.01;
+beta=100;
+Rc=4;
+Re=0.4;
+Rb1=110;
+Rb2=10;
+Rl=4;
+f=100;
+Ie=2;
+valoare=1;
+
+T=1/f; 
+
+interface(A,VAl,beta,Rc,Re,Rb1,Rb2,Rl,f,Ie,valoare)
